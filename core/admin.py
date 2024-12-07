dd750210611d7b562b3a35b8b7836b039a7277f1
leondvieira <analaurasilvamuller@gmail.com>
2024-12-07 18:58:22 -0300

    Customização do Admin

--- core/admin.py
+++ core/admin.py
@@ -6,9 +6,14 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 
-from core import models
+from core.models.autor import Autor
+from core.models.categoria import Categoria
+from core.models.editora import Editora
+from core.models.livro import Livro
+from core.models.user import User
 
 
+@admin.register(User)
 class UserAdmin(BaseUserAdmin):
     """Define the admin pages for users."""
 
@@ -51,7 +56,37 @@ class UserAdmin(BaseUserAdmin):
     )
 
 
-admin.site.register(models.User, UserAdmin)
-admin.site.register(models.Categoria)
-admin.site.register(models.Editora)
-admin.site.register(models.Livro)
+@admin.register(Autor)
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'email')
+    search_fields = ('nome', 'email')
+    list_filter = ('nome',)
+    ordering = ('nome', 'email')
+    list_per_page = 10
+
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('descricao',)
+    search_fields = ('descricao',)
+    list_filter = ('descricao',)
+    ordering = ('descricao',)
+    list_per_page = 10
+
+
+@admin.register(Editora)
+class EditoraAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'site')
+    search_fields = ('nome', 'site')
+    list_filter = ('nome', 'site')
+    ordering = ('nome', 'site')
+    list_per_page = 10
+
+
+@admin.register(Livro)
+class LivroAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'editora', 'categoria')
+    search_fields = ('titulo', 'editora__nome', 'categoria__descricao')
+    list_filter = ('editora', 'categoria')
+    ordering = ('titulo', 'editora', 'categoria')
+    list_per_page = 25
--- core/models/autor.py
+++ core/models/autor.py
@@ -3,3 +3,4 @@ from django.db import models
 
 class Autor(models.Model):
     nome = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
